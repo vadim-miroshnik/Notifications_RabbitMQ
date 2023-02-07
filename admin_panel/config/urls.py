69e757989d7a8112fb57e3c0b62b08ba3662ff1d
dovgopol.di <dmitrij.dowgopol@eltex.loc>
2023-02-08 00:03:44 +0700

    add models and admin

--- admin_panel/config/urls.py
+++ admin_panel/config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+
+from config.settings import STATIC_URL, STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(STATIC_URL, document_root=STATIC_ROOT)
