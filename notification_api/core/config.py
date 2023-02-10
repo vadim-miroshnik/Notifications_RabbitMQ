c3b770748766229ec59f14ade74566705c7f58f3
dovgopol.di <dmitrij.dowgopol@eltex.loc>
2023-02-11 00:33:08 +0700

    some fix

--- notification_api/core/config.py
+++ notification_api/core/config.py
@@ -25,13 +25,19 @@ class Postgres(BaseSettings):
     port: int = Field(5432)
     db: str = Field("movies_database")
     user: str = Field("app")
-    pwd: str = Field("123qwe")
+    password: str = Field("123qwe")
+
+
+class Rabbit(BaseSettings):
+    host: str = Field("127.0.0.1")
+    port: int = Field(5672)
 
 
 class Settings(BaseSettings):
     notify_app: NotifyApp = Field(NotifyApp())
     mongo: Mongo = Field(Mongo())
     postgres: Postgres = Field(Postgres())
+    rabbit: Rabbit = Field(Rabbit())
     project_name: str = Field("notification")
     debug: bool = Field(False)
 
