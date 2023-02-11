e2a728713c231cefb2e724be8404769d80cf236c
Viatcheslav Pestov <pestov74@gmail.com>
2023-02-11 11:42:52 +0500

    migration (user + template)

--- admin_panel/movies/models.py
+++ admin_panel/movies/models.py
@@ -51,10 +51,15 @@ class Filmwork(UUIDMixin, TimeStampedMixin):
     type = models.CharField(verbose_name=_("types"), max_length=7, choices=TypeChoice.choices, default=TypeChoice.movie)
 
 
-class User(UUIDMixin):
+class User(UUIDMixin, TimeStampedMixin):
     login = models.CharField(verbose_name=_("login"), max_length=63)
     password = models.TextField(verbose_name=_("password"), blank=True, null=True)
     fullname = models.CharField(verbose_name=_("fullname"), max_length=127, null=True)
     email = models.CharField(verbose_name=_("email"), max_length=63, null=True)
     phone = models.CharField(verbose_name=_("phone"), max_length=31, null=True)
     subscribed = models.BooleanField(verbose_name=_("subscribed"), default=False)
+    confirmed = models.BooleanField(verbose_name=_("confirmed"), default=False)
+
+
+class Template(UUIDMixin, TimeStampedMixin):
+    template = models.TextField(verbose_name=_("template"), blank=True, null=True)
