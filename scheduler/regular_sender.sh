b5045532ffcafffe473746146b4106a7fc65aaea
dovgopol.di <dmitrij.dowgopol@eltex.loc>
2023-02-17 01:23:27 +0700

    add_scheduler

--- scheduler/regular_sender.sh
+++ scheduler/regular_sender.sh
@@ -1,4 +1,6 @@
-curl -X POST "http://127.0.0.1:8000/api/v1/notifications/add-group" \
+#!/bin/bash
+curl -X POST "http://$NOTIFICATION_API:$NOTIFICATION_PORT/api/v1/notifications/add-group" \
   -H  "accept: application/json" \
   -H  "Content-Type: application/json" \
-  -d "{  \"template_id\": \"7dfba2c1-4057-4ef7-a85d-452aae23d428\",  \"group_id\": \"9fe7e975-a8e9-4387-9dc3-c92770ffd1cb\"}"
+  -d "{  \"template_id\": \"7dfba2c1-4057-4ef7-a85d-452aae23d428\",  \"group_id\": \"9fe7e975-a8e9-4387-9dc3-c92770ffd1cb\"}" \
+  >> /var/log/syslog
