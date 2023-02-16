2281b5989b9ee260375f728db566f4b59e63fef9
fall3n <aka.fall3n@gmail.com>
2023-02-16 21:44:59 +0300

    priority queues, multiple recipients

--- worker/models.py
+++ worker/models.py
@@ -4,13 +4,28 @@ from pydantic import BaseModel
 
 
 class NotifTypeEnum(str, Enum):
-    EMAIL = 'email'
-    WEBSOCKET = 'websocket'
+    EMAIL = "email"
+    WEBSOCKET = "websocket"
+
+
+class PriorityEnum(str, Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
+
+class Recipient(BaseModel):
+    email: str
+    phone: str | None = None
+    fullname: str | None = None
+    data: dict | None = None
+    timezone: int = 0
 
 
 class Notification(BaseModel):
+    id: str
     notif_type: NotifTypeEnum = NotifTypeEnum.EMAIL
     subject: str | None = None
     template: str | None = None
-    content_data: list[dict] = []
-    recepients: list[str] = []
+    recipients: list[Recipient] = []
+    priority: PriorityEnum = PriorityEnum.LOW
--- worker/test_sender.py
+++ worker/test_sender.py
@@ -2,7 +2,7 @@ import asyncio
 import aio_pika
 from config import settings
 import orjson
-from models import Notification, NotifTypeEnum
+from models import Notification, NotifTypeEnum, PriorityEnum, Recipient
 
 
 async def main() -> None:
@@ -14,13 +14,17 @@ async def main() -> None:
     )
     async with connection:
         channel = await connection.channel()
-        exchange = await channel.declare_exchange(name="Ex")
-        msg = Notification(notif_type=NotifTypeEnum.EMAIL,
+        exchange = await channel.declare_exchange(name="notifications")
+        msg = Notification(id="",
+                           notif_type=NotifTypeEnum.EMAIL,
                            subject="subj",
                            template="{{ title }} {{ text }}",
-                           content_data=[{"title": "Hello", "text": "test"}, ],
-                           recepients=["aka.fall3n@gmail.com"])
-        await exchange.publish(message=aio_pika.Message(body=orjson.dumps(msg.dict())), routing_key="messages")
+                           recipients=[Recipient(email="aka.fall3n@gmail.com", data={"title": "Hello Vadim", "text": "test"}),
+                                       Recipient(email="aka.fall4n@gmail.com", data={"title": "Hello Maxim", "text": "test"})],
+                           priority=PriorityEnum.HIGH)
+        queue = await channel.declare_queue(msg.priority, durable=True)
+        await queue.bind(exchange=exchange.name)
+        await exchange.publish(message=aio_pika.Message(body=orjson.dumps(msg.dict())), routing_key=queue.name)
 
 
 if __name__ == "__main__":
