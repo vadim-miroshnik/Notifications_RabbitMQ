414119b8da92c2424b362f7a5ac335965ce71023
Viatcheslav Pestov <pestov74@gmail.com>
2023-02-17 21:20:40 +0500

    fix 7

--- notification_api/main.py
+++ notification_api/main.py
@@ -50,7 +50,7 @@ async def startup_event():
 
 @app.on_event("shutdown")
 async def shutdown_event():
-    rabbitmq.close()
+    await rabbitmq.close()
     db.close()
     await mongodb.mongodb.close()
 
