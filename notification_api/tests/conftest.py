c7ac7cc71e528084c8dd98bf2df1feb88c607304
dovgopol.di <dmitrij.dowgopol@eltex.loc>
2023-02-19 03:39:02 +0700

    change db to postgress

--- notification_api/tests/conftest.py
+++ notification_api/tests/conftest.py
@@ -1,7 +1,4 @@
-import datetime
-import os
 import sys
-import uuid
 from pathlib import Path
 from typing import Any
 from typing import Generator
@@ -9,40 +6,29 @@ from typing import Generator
 import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine, Column, Text, String, Boolean, TIMESTAMP, Integer
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 sys.path.append(str(Path(__file__).parent.parent.resolve()))
 
-from db.postgres import Base, db
-from db import postgres
-from api.v1.users import router as users_router
 from models.user import User
+from api.v1.users import router as users_router
+from core.config import settings
 
 BASE_DIR = Path(__file__).parent.resolve()
 
-def start_application():
-    app = FastAPI()
-    app.include_router(users_router)
-    return app
+POSTGRES_URL = f"postgresql://{settings.postgres.user}:{settings.postgres.password}@{settings.postgres.host}:{settings.postgres.port}/{settings.postgres.db}"
 
+engine = create_engine(POSTGRES_URL, echo=True)
 
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{BASE_DIR}/test_db.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-# Use connect_args parameter only with sqlite
 SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 @pytest.fixture(scope="function")
-def app() -> Generator[FastAPI, Any, None]:
-    """
-    Create a fresh database on each test case.
-    """
-    Base.metadata.create_all(engine)  # Create the tables.
-    _app = start_application()
-    yield _app
-    Base.metadata.drop_all(engine)
+def app() -> FastAPI:
+    app = FastAPI()
+    app.include_router(users_router)
+    return app
 
 
 @pytest.fixture(scope="function")
@@ -50,7 +36,6 @@ def db_session(app: FastAPI) -> Generator[SessionTesting, Any, None]:
     connection = engine.connect()
     transaction = connection.begin()
     session = SessionTesting(bind=connection)
-    postgres.db = session
     yield session  # use the session in tests.
     session.close()
     transaction.rollback()
@@ -59,8 +44,27 @@ def db_session(app: FastAPI) -> Generator[SessionTesting, Any, None]:
 
 @pytest.fixture(scope="function")
 def client(app: FastAPI, db_session: SessionTesting) -> Generator[TestClient, Any, None]:
-    app.dependency_overrides[db] = db_session
-
 
     with TestClient(app) as client:
         yield client
+
+
+@pytest.fixture(scope="function")
+def test_user_info(db_session):
+    test_user_name = "test_user_2"
+    user = User(
+        login=test_user_name,
+        password="password",
+        email="testuser@example.com",
+        fullname="fullname",
+        phone="+79627950693",
+    )
+    db_session.add(user)
+    db_session.commit()
+
+    def inner():
+        result = db_session.query(User).filter_by(login=test_user_name).all()[0]
+        return result
+
+    yield inner
+    db_session.query(User).filter_by(login=test_user_name).delete()
--- notification_api/tests/unit/test_default.py
+++ notification_api/tests/unit/test_default.py
@@ -1,34 +1,16 @@
 from http import HTTPStatus
 
-from models.user import User
 
-
-def test_1(client, db_session):
-    user = User(
-        login="test_user",
-        password="password",
-        email="testuser@example.com",
-        fullname="fullname",
-        phone="+79627950693",
+def test_enable_notification(client, test_user_info):
+    response = client.get(
+        "/login",
+        params={
+            "user_id": test_user_info().id,
+        },
     )
-    db_session.add(user)
-    db_session.commit()
-    user = db_session.query(User).filter_by(login="test_user").all()[0]
-    print(user.id)
-    print(user.allow_send_email)
-    #response = client.post("/login")
-    data = {
-        "user_id": user.id,
-    }
-    response = client.get("/login", params=data)
-    print(response.json())
     assert response.status_code == HTTPStatus.OK
     assert "access_token" in response.json()
-    acess_token = response.json()['access_token']
-    print(acess_token)
-    assert True
+    acess_token = response.json()["access_token"]
     response = client.get("/enable-notifications", headers={"Authorization": f"Bearer {acess_token}"})
     assert response.status_code == HTTPStatus.OK
-    user = db_session.query(User).filter_by(login="test_user").all()[0]
-    print(user.allow_send_email)
-    assert user.allow_send_email
+    assert test_user_info().allow_send_email is True
